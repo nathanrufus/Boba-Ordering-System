635201009b26b0bf1e476c9c2d19bd8f506391a6
nathanrufus <nathanrufus540@gmail.com>
2026-02-22 17:38:51 +0300

    feat:frontend setup

---
+++ boba-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-amber-800">
+      hello world
+    </div>
+  )
+}
+
+export default App
