98be9aac4cc4efb49cea5f8b76437b041bf2f5d6
nathanrufus <nathanrufus540@gmail.com>
2026-02-22 10:29:19 +0300

    feat:made sure the database is connected properly

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const { errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 
@@ -11,8 +12,16 @@ app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 
-app.get("/health", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ status: "ok" });
 });
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ error: "NOT_FOUND", message: "Route not found" });
+});
+
+// Error handler (must be last)
+app.use(errorHandler);
 
 module.exports = app;
